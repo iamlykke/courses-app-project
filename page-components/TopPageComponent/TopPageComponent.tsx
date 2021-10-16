8525d9c64af6e5550640d35bde527bb8400b7e67
sergeiusachev <usachev97@gmail.com>
2021-10-16 15:41:32 +0300

    feat(dev_sausachev_98): complete lesson 98

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,42 +1,72 @@
-import styles from './TopPageComponent.module.css';
-import {TopPageComponentProps} from './TopPageComponent.props';
-import {Advantages, HhData, Htag, Product, Sort, Tag} from '../../components';
-import {TopLevelCategory} from '../../interfaces/page.interface';
-import {SortEnum} from "../../components/Sort/Sort.props";
-import {useReducer} from "react";
-import {sortReducer} from "./sort.reducer";
+import styles from "./TopPageComponent.module.css";
+import { TopPageComponentProps } from "./TopPageComponent.props";
+import { Advantages, HhData, Htag, Product, Sort, Tag } from "../../components";
+import { TopLevelCategory } from "../../interfaces/page.interface";
+import { SortEnum } from "../../components/Sort/Sort.props";
+import { useEffect, useReducer } from "react";
+import { sortReducer } from "./sort.reducer";
 
-export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
-    const [{products: sortedProducts, sort}, dispatchSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating });
-    const setSort = (sort: SortEnum) => {
-        dispatchSort({type: sort});
-    };
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.title}>
-                <Htag tag='h1'>{page.title}</Htag>
-                {products && <Tag color='grey' size='m'> {products.length} </Tag>}
-                <Sort sort={sort} setSort={setSort}/>
-            </div>
-            <div>
-                {sortedProducts && sortedProducts.map(p => (
-                    <Product key={p._id} product={p}/>
-                ))}
-            </div>
-            <div className={styles.hhTitle}>
-                <Htag tag='h2'> Вакансии: {page.category}</Htag>
-                <Tag color='red' size='m'>hh.ru</Tag>
-            </div>
-            {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
-            {page.advantages && page.advantages.length > 0 &&
-                <>
-                    <Htag tag='h2'>Преимущества</Htag>
-                    <Advantages advantages={page.advantages} />
-                </>
-            }
-            {page.seoText &&  <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
-            <Htag tag='h2'>Получаемые навыки</Htag>
-            {page.tags.map(tag => <Tag key={tag} color='primary'>{tag}</Tag>)}
-        </div>
-    );
+export const TopPageComponent = ({
+  page,
+  products,
+  firstCategory,
+}: TopPageComponentProps): JSX.Element => {
+  const [{ products: sortedProducts, sort }, dispatchSort] = useReducer(
+    sortReducer,
+    { products, sort: SortEnum.Rating }
+  );
+
+  const setSort = (sort: SortEnum) => {
+    dispatchSort({ type: sort });
+  };
+
+  useEffect(() => {
+    dispatchSort({ type: "reset", initialState: products });
+  }, [products]);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m">
+            {" "}
+            {products.length}{" "}
+          </Tag>
+        )}
+        <Sort sort={sort} setSort={setSort} />
+      </div>
+      <div>
+        {sortedProducts &&
+          sortedProducts.map((p) => <Product key={p._id} product={p} />)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2"> Вакансии: {page.category}</Htag>
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
+      </div>
+      {firstCategory == TopLevelCategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
+      {page.advantages && page.advantages.length > 0 && (
+        <>
+          <Htag tag="h2">Преимущества</Htag>
+          <Advantages advantages={page.advantages} />
+        </>
+      )}
+      {page.seoText && (
+        <div
+          className={styles.seo}
+          dangerouslySetInnerHTML={{ __html: page.seoText }}
+        />
+      )}
+      <Htag tag="h2">Получаемые навыки</Htag>
+      {page.tags.map((tag) => (
+        <Tag key={tag} color="primary">
+          {tag}
+        </Tag>
+      ))}
+    </div>
+  );
 };
