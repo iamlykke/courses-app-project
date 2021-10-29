119c6584df4562fa3ef17588d1f01627c95f79c3
sergeiusachev <usachev97@gmail.com>
2021-10-29 20:06:28 +0300

    feat(dev_sausachev_108): complete 108 lesson

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,23 +1,35 @@
 import { ButtonProps } from "./Button.props";
-import styles from './Button.module.css';
-import ArrowIcon from './arrow.svg';
-import cn from 'classnames';
+import styles from "./Button.module.css";
+import ArrowIcon from "./arrow.svg";
+import cn from "classnames";
+import { motion } from "framer-motion";
 
-export const Button = ({ children, arrow = 'none', appearence, className, ...props }: ButtonProps): JSX.Element => {
-    return (
-        <button 
-            className={cn(styles.button, className, {
-                [styles.primary]: appearence === 'primary',
-                [styles.ghost]: appearence === 'ghost',
-        })}
-            {...props}
-        > 
-            {children}
-            {arrow !== 'none' &&   <span className={cn(styles.arrow, {
-                [styles.down]: arrow === 'down'
-            })}> 
-                <ArrowIcon />
-            </span> }
-         </button>
-    );
+export const Button = ({
+  children,
+  arrow = "none",
+  appearence,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  return (
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      className={cn(styles.button, className, {
+        [styles.primary]: appearence === "primary",
+        [styles.ghost]: appearence === "ghost",
+      })}
+      {...props}
+    >
+      {children}
+      {arrow !== "none" && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow === "down",
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
+    </motion.button>
+  );
 };
