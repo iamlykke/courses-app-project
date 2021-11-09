b3b42f7c325d3cf5917365d55b09b9383c876427
sergeiusachev <usachev97@gmail.com>
2021-11-09 23:49:09 +0300

    feat(dev_sausachev_132): refactor docker-compose and configs

--- next.config.js
+++ next.config.js
@@ -4,28 +4,20 @@ module.exports = {
   },
   webpack(config, options) {
     config.module.rules.push({
-      test: /\.svg?$/,
-      oneOf: [
-        {
-          use: [
-            {
-              loader: "@svgr/webpack",
-              options: {
-                prettier: false,
-                svgo: true,
-                svgoConfig: {
-                  plugins: [{ removeViewBox: false }],
-                },
-                titleProp: true,
-              },
-            },
-          ],
-          issuer: {
-            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-          },
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{
+            removeViewBox: false
+          }],
         },
-      ],
+        titleProp: true,
+      },
+      test: /\.svg$/,
     });
+
     return config;
   },
 };
