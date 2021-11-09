fb6f78870b7bfa513b77ef1dca4ae8709bb037d7
sergeiusachev <usachev97@gmail.com>
2021-11-09 19:35:14 +0300

    feat(dev_sausachev_127): add yandex-metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,21 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import Head from "next/head";
 import React from "react";
 import "../styles/globals.css";
+import ym from "react-yandex-metrika";
+import { YMInitializer } from "react-yandex-metrika";
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on("routeChangeComplete", (url: string) => {
+    if (typeof window !== "undefined") {
+      ym("hit", url);
+    }
+  });
   return (
     <>
       <Head>
         <title>MyTop</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
           rel="stylesheet"
@@ -19,6 +27,11 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         />
         <meta property="og:locale" content={"ru_RU"} />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
