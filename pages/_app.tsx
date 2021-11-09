0c6c790c4af5346a77f2166b565cd886e55ab93f
sergeiusachev <usachev97@gmail.com>
2021-11-09 19:27:56 +0300

    feat(dev_sausachev_126): add meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,21 @@ import Head from "next/head";
 import React from "react";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <title>MyTop</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta property="og:locale" content={"ru_RU"} />
       </Head>
       <Component {...pageProps} />
     </>
