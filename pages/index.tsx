ab070a6c689ef06f533de22b8e7ab1c70fe35601
sergeiusachev <usachev97@gmail.com>
2021-11-09 21:04:58 +0300

    feat(dev_sausachev_130): add 404 and 500 pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,6 @@ import axios from "axios";
 import { MenuItem } from "../interfaces/menu.interface";
 import { API } from "../helpers/api";
 
-function Home({ menu }: HomeProps): JSX.Element {
-  const [rating, setRating] = useState<number>(4);
-
-  return <></>;
-}
-
-export default withLayout(Home);
-
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const firstCategory = 0;
   const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
